d074e89524cb70864111fffc5ef50a94856b07f7
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-08-10 09:22:22 -0300

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct worker_info
+{
+    char name[50];
+    int age;
+    float salary;
+};
+
+int main()
+{
+    FILE *archive;
+    archive = fopen("worker_info.txt", "w");
+    if(archive == NULL)
+    {
+        printf("ERROR!");
+    }
+
+    struct worker_info worker;
+
+    printf("\nEnter the worker name: ");
+    fgets(worker.name, 50, stdin);
+    fflush(stdin);
+
+    printf("\nEnter the worker age: ");
+    scanf("%d", &worker.age);
+
+    printf("\nEnter the worker salary: ");
+    scanf("%f", &worker.salary);
+
+    printf("\n");
+
+    fprintf(archive, "Name: %s", worker.name);
+    fprintf(archive, "Age: %d", worker.age);
+    fprintf(archive, "\nSalary: %.2f", worker.salary);
+    
+    fclose(archive);
+    
+    return 0;
+}
