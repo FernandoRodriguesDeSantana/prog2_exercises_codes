f0fb079ab82171d9c640c6e5bddbff69da4130b7
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-09-14 08:29:58 -0300

    Add files via upload

---
+++ FERNANDO_PROVAPROGII/Q1/linear_queue.c
@@ -0,0 +1,68 @@
+#include "linear_queue.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+Node *buildNode(int value)
+{
+    //allocating memory
+    Node *newNode = malloc(sizeof(Node));
+    if(newNode == NULL)
+    {
+        printf("\nERROR IN THE MEMORY ALLOCATION");
+        return NULL;
+    }
+
+    //assigning a value to variable "value" and "next"
+    newNode -> value = value;
+    newNode -> next = NULL;
+    return newNode;
+}
+
+void enqueuee(Queuee *queuee, int value)
+{
+    Node *newNode = buildNode(value);
+
+    if(queuee -> tail == NULL)
+    {
+        queuee -> head = newNode;
+        queuee -> tail = newNode;
+    } else
+    {
+        queuee -> tail -> next = newNode;
+        queuee -> tail = newNode;
+    }
+}
+
+void printQueuee(Queuee *queuee)
+{
+    // start at the first position
+    Node *actual = queuee -> head;
+    while(actual != NULL)
+    {
+        printf("%d ", actual -> value);
+        // start position ++
+        actual = actual -> next;
+    }
+    printf("\n");
+}
+
+
+int desenqueuee(Queuee *queuee)
+{ if(queuee -> head == NULL)
+    {
+        return 1;
+    } else
+    {
+        Node *actual = queuee -> head;
+        queuee -> head = queuee -> head -> next;
+        free(actual);
+    }
+}
+
+void printFirstQueuee(Queuee *queuee)
+{
+    Node *actual = queuee -> head;
+    printf("%d ", actual -> value);
+}
+
