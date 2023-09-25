ef46b4eb35d7f19c618bd5988960af0bd0b89ef3
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-09-25 17:17:00 -0300

    Delete calculator directory

--- calculator/calculator.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALCULATOR_H
-
-#define CALCULATOR_H
-
-double sum_numbers(double a, double b);
-double sub_numbers(double a, double b);
-double mul_numbers(double a, double b);
-
-#endif // CALCULATOR_H
