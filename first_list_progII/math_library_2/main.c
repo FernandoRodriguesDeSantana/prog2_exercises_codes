233c09fcaf6081095ee9fdf9ed4601929b3f9cb9
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-09-25 20:05:32 -0300

    Update main.c

--- first_list_progII/math_library_2/main.c
+++ first_list_progII/math_library_2/main.c
@@ -1,3 +1,5 @@
+// Crie uma biblioteca de matemática chamada biblio_mat.h que deve possuir as seguintes funções:
+//a. fatorial b. valor absoluto c. quadrado Faça a modularização incluindo biblio_math.c e main.c.
 #include <stdio.h>
 #include <stdlib.h>
 #include "lib_mat.h"
