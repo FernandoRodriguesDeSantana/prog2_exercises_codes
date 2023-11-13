fcd52b2366d29d7ec9e8eafa936acb93ecb5d9bc
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-11-13 09:01:12 -0300

    Update main.c

--- finalProject/main.c
+++ finalProject/main.c
@@ -1,29 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+
 #include "functions.h"
 
 // 100 sorted persons
 
 int main(){
 
-    char buffer[1024];
-    char *data;
-
     FILE *database = fopen("microdados_enem_2016_coma.csv", "r");
 
     if(!database)
         printf("\nERROR: could not open file\n");
-    else
+    else{
         printf("\nI was able to open the file\n");
+        char buffer[1024];
+        int row, column = 0;
 
-    fgets(buffer, sizeof(buffer), database);
-    printf("%s\n", buffer);
+        while(fgets(buffer, sizeof(buffer), database)){
+            column = 0;
+            row++;
 
-    while(fgets(buffer, sizeof(buffer), database))
-        printf("%s\n", buffer);
+            if(row == 1)
+                continue;
 
+            char *data = strtok(buffer, ", ");
 
-    strtok(buffer, ",");
+            while(data){
+                if(column == 1)
+                    printf("\n\tName: ");
+
+                if(column == 2)
+                    printf("\n\tName: ");
+
+                printf("%s", data);
+                data = strtok(NULL, ", ");
+                column++;
+            }
+            printf("\n");
+        }
+        fclose(database);
+    }
 
 
     return 0;
