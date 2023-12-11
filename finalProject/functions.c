a28400d818168c92959b485527c682505403e913
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-12-10 22:12:16 -0300

    Add files via upload

--- finalProject/functions.c
+++ finalProject/functions.c
@@ -4,7 +4,8 @@
 #include <string.h>
 #include <time.h>
 #include "functions.h"
-struct StudentData {
+struct StudentData
+{
     int NU_INSCRICAO;
     char SG_UF_RESIDENCIA[3];
     int NU_IDADE;
@@ -20,55 +21,121 @@ struct StudentData {
     char Q025;
 };
 
-void readInfo(char *database, int MAX_RANDOM_LINES){
-        char buffer[1024];
+void readInfo(char *database, int MAX_RANDOM_LINES)
+{
+    struct StudentData *student[MAX_RANDOM_LINES];
+    char buffer[1024];
 
-        int row = 0, column = 0, count;
+    int row = 0, column = 0, count=0;
 
-        int printColumns[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    int printColumns[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 
-        srand(time(NULL));
+    srand(time(NULL));
 
-        while (fgets(buffer, sizeof(buffer), database) && row < MAX_RANDOM_LINES) {
-            column = 0;
-            row++;
+    while (fgets(buffer, sizeof(buffer), database) && row < MAX_RANDOM_LINES)
+    {
+        column = 0;
+        row++;
 
-            if (row == 1)
-                continue;
+        if (row == 1)
+            continue;
 
-            char *data = strtok(buffer, ", ");
+        char *data = strtok(buffer, ", ");
 
-            while (data) {
-                if (column < sizeof(printColumns) / sizeof(printColumns[0]) && printColumns[column]) {
-                    if (column == 0)
-                        printf("\n\tSerial code: ");
-                    else if (column == 1)
-                        printf("\n\tState: ");
-                    else if (column == 2)
-                        printf("\n\tAge: ");
-                    else if (column == 3)
-                        printf("\n\tGender: ");
-                    else if (column == 4)
-                        printf("\n\tColor: ");
-                    else if (column == 5)
-                        printf("\n\tNatural Sciences Score: ");
-                    else if (column == 6)
-                        printf("\n\tHuman Sciences Score: ");
-                    else if (column == 7)
-                        printf("\n\tLanguages Score: ");
-                    else if (column == 8)
-                        printf("\n\tMath Score: ");
-                    else if (column == 9)
-                        printf("\n\tEssay Score: ");
-                    else if (column == 10)
-                        printf("\n\tAnswers: ");
-
-                    printf("%s ", data);
+        while (data)
+        {
+            if (column < sizeof(printColumns) / sizeof(printColumns[0]) && printColumns[column])
+            {
+                if (column == 0)
+                {
+                    printf("\n\tSerial code: ");
                 }
 
-                data = strtok(NULL, ", ");
-                column++;
+                else if (column == 1)
+                {
+                    printf("\n\tState: ");
+                }
+
+                else if (column == 2)
+                {
+                    printf("\n\tAge: ");
+                }
+
+                else if (column == 3)
+                {
+                    printf("\n\tGender: ");
+                }
+
+                else if (column == 4)
+                {
+                    printf("\n\tColor: ");
+                }
+
+                else if (column == 5)
+                {
+                    printf("\n\tNatural Sciences Score: ");
+                }
+
+                else if (column == 6)
+                {
+                    printf("\n\tHuman Sciences Score: ");
+                }
+
+                else if (column == 7)
+                {
+                    printf("\n\tLanguages Score: ");
+                }
+
+                else if (column == 8)
+                {
+                    printf("\n\tMath Score: ");
+                }
+
+                else if (column == 9)
+                {
+                    printf("\n\tEssay Score: ");
+                }
+
+                else if (column == 10)
+                {
+                    printf("\n\tAnswer 1: ");
+                }
+                else if (column == 11)
+                {
+                    printf("\n\tAnswer 2: ");
+                }
+                else if (column == 12)
+                {
+                    printf("\n\tAnswer 3: ");
+                }
+                printf("%s ", data);
             }
-            printf("\n");
         }
+        data = strtok(NULL, ", ");
+        column++;
+    }
+    printf("\n");
 }
+
+void storeData(FILE *database, struct StudentData *student, int MAX_RANDOM_LINES) {
+    char buffer[1024];
+    int count = 0;
+
+    fgets(buffer, sizeof(buffer), database); // Read and discard the header line
+
+    fgets(buffer, sizeof(buffer), database);
+    while (fgets(buffer, sizeof(buffer), database) && count < MAX_RANDOM_LINES) {
+        sscanf(buffer, "%d,%2s,%d,%c,%d,%f,%f,%f,%f,%f,%d,%f,%c",
+        &student[count]->NU_INSCRICAO, student[count]->SG_UF_RESIDENCIA,
+        &student[count]->NU_IDADE, &student[count]->TP_SEXO,
+        &student[count]->TP_COR_RACA, &student[count]->NU_NOTA_CN,
+        &student[count]->NU_NOTA_CH, &student[count]->NU_NOTA_LC,
+        &student[count]->NU_NOTA_MT, &student[count]->NU_NOTA_REDACAO,
+        &student[count]->Q005, &student[count]->Q006, &student[count]->Q025);
+        count++;
+    }
+}
+
+
+
+    }
--- finalProject/functions.h
+++ finalProject/functions.h
@@ -2,5 +2,6 @@
 #define FUNCTIONS_H
 
 void readInfo(char *database, int MAX_RANDOM_LINES);
+void storeData(FILE *database, struct StudentData *students, int MAX_RANDOM_LINES)
 #endif // FUNCTIONS_H
 
