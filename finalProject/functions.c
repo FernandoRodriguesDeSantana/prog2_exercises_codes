753254d7a599100ffdf9cd665c1209df4805fdcc
Fernando Rodrigues de Santana <fernandobr11sp@gmail.com>
2023-12-12 12:34:38 -0300

    Update functions.c

--- finalProject/functions.c
+++ finalProject/functions.c
@@ -1,39 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 #include <string.h>
 #include <time.h>
 #include "functions.h"
-struct StudentData
-{
-    int NU_INSCRICAO;
-    char SG_UF_RESIDENCIA[3];
-    int NU_IDADE;
-    char TP_SEXO;
-    int TP_COR_RACA;
-    float NU_NOTA_CN;
-    float NU_NOTA_CH;
-    float NU_NOTA_LC;
-    float NU_NOTA_MT;
-    float NU_NOTA_REDACAO;
-    int Q005;
-    float Q006;
-    char Q025;
+struct Student {
+    char serialCode[50];
+    int answer1;
+    int answer2;
+    int answer3;
+    float average;
 };
 
-void readInfo(char *database, int MAX_RANDOM_LINES)
-{
-    struct StudentData *student[MAX_RANDOM_LINES];
+int charToNumber(char character) {
+    if (character == 'A') {
+        return 0;
+    }
+    else if (character == 'B') {
+        return 800;
+    }
+    else if (character == 'C') {
+        return 1100;
+    }
+    else if (character == 'D') {
+        return 1500;
+    }
+    else if (character == 'E') {
+        return 2000;
+    }
+    else if (character == 'F') {
+        return 2300;
+    }
+    else if (character == 'G') {
+        return 3100;
+    }
+    else if (character == 'H') {
+        return 4000;
+    }
+    else if (character == 'I') {
+        return 4700;
+    }
+    else if (character == 'J') {
+        return 5500;
+    }
+    else if (character == 'K') {
+        return 6300;
+    }
+    else if (character == 'L') {
+        return 7000;
+    }
+    else if (character == 'M') {
+        return 7800;
+    }
+    else if (character == 'N') {
+        return 9500;
+    }
+    else if (character == 'O') {
+        return 12000;
+    }
+    else if (character == 'P') {
+        return 15000;
+    }
+    else if (character == 'Q') {
+        return 20000;
+    }
+    else {
+        return -1;
+    }
+}
+
+int charToBin(char character) {
+    if (character == 'A') {
+        return 0;
+    }
+    else if (character == 'B') {
+        return 1;
+    }
+    else {
+        return -1;
+    }
+}
+void readInfo(FILE *database, int MAX_RANDOM_LINES) {
     char buffer[1024];
-
-    int row = 0, column = 0, count=0;
-
-    int printColumns[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    float aux1 = 0, aux2 = 0, aux3 = 0, aux4 = 0, aux5 = 0, average = 0;
+    int row = 0, column = 0, answer1 = 0, answer2 = 0, answer3 = 0, studentIndex = 0;
+    struct Student students[100];
+    int printColumns[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
 
     srand(time(NULL));
+    rewind(database);
 
-    while (fgets(buffer, sizeof(buffer), database) && row < MAX_RANDOM_LINES)
-    {
+    while (fgets(buffer, sizeof(buffer), database) && row < MAX_RANDOM_LINES) {
         column = 0;
         row++;
 
@@ -42,100 +98,100 @@ void readInfo(char *database, int MAX_RANDOM_LINES)
 
         char *data = strtok(buffer, ", ");
 
-        while (data)
-        {
-            if (column < sizeof(printColumns) / sizeof(printColumns[0]) && printColumns[column])
-            {
-                if (column == 0)
-                {
-                    printf("\n\tSerial code: ");
-                }
+        while (data) {
+                if (column < sizeof(printColumns) / sizeof(printColumns[0]) && printColumns[column]) {
 
-                else if (column == 1)
-                {
-                    printf("\n\tState: ");
-                }
+                    if (column == 0)
+                    {
+                        printf("\n\tSerial code: ");
+                        printf("%s ", data);
+                        strncpy(students[studentIndex].serialCode, data, sizeof(students[studentIndex].serialCode));
+                    }
 
-                else if (column == 2)
-                {
-                    printf("\n\tAge: ");
-                }
+                    else if (column == 1){
+                        printf("\n\tState: ");
+                        printf("%s ", data);
+                    }
 
-                else if (column == 3)
-                {
-                    printf("\n\tGender: ");
-                }
+                    else if (column == 2){
+                        printf("\n\tAge: ");
+                        printf("%s ", data);
+                    }
 
-                else if (column == 4)
-                {
-                    printf("\n\tColor: ");
-                }
+                    else if (column == 3){
+                        printf("\n\tGender: ");
+                        printf("%s ", data);
+                    }
+                    else if (column == 4){
+                        printf("\n\tColor: ");
+                        printf("%s ", data);
+                    }
+                    else if (column == 5){
+                        printf("\n\tNatural Sciences Score: ");
+                        aux1 = atof(data);
+                        printf("%s ", data);
+                    }
 
-                else if (column == 5)
-                {
-                    printf("\n\tNatural Sciences Score: ");
-                }
+                    else if (column == 6){
+                        printf("\n\tHuman Sciences Score: ");
+                        aux2 = atof(data);
+                        printf("%s ", data);
+                    }
 
-                else if (column == 6)
-                {
-                    printf("\n\tHuman Sciences Score: ");
-                }
+                    else if (column == 7){
+                        printf("\n\tLanguages Score: ");
+                        aux3 = atof(data);
+                        printf("%s ", data);
+                    }
 
-                else if (column == 7)
-                {
-                    printf("\n\tLanguages Score: ");
-                }
+                    else if (column == 8){
+                        printf("\n\tMath Score: ");
+                        aux4 = atof(data);
+                        printf("%s ", data);
+                    }
 
-                else if (column == 8)
-                {
-                    printf("\n\tMath Score: ");
-                }
+                    else if (column == 9){
+                        printf("\n\tEssay Score: ");
+                        aux5 = atof(data);
+                        printf("%s ", data);
+                    }
 
-                else if (column == 9)
-                {
-                    printf("\n\tEssay Score: ");
-                }
+                    else if (column == 10){
+                        printf("\n\tAnswers: ");
+                        answer1 = atoi(data);
+                        printf("\n\t\tAnswer 1: ");
+                        printf("%s ", data);
 
-                else if (column == 10)
-                {
-                    printf("\n\tAnswer 1: ");
+                    }
+
+                    else if (column == 11){
+                        printf("\n\t\tAnswer 2: ");
+                        answer2 = charToNumber(data[0]);
+                        printf("%s ", data);
+                    }
+
+                    else if (column == 12){
+                        printf("\n\t\tAnswer 3: ");
+                        answer3 = charToBin(data[0]);
+                        printf("%s ", data);
+                        printf("\n");
+                    }
+                    data = strtok(NULL, ", ");
                 }
-                else if (column == 11)
-                {
-                    printf("\n\tAnswer 2: ");
-                }
-                else if (column == 12)
-                {
-                    printf("\n\tAnswer 3: ");
-                }
-                printf("%s ", data);
-            }
+                column++;
         }
-        data = strtok(NULL, ", ");
-        column++;
+        average = (aux1+aux2+aux3+aux4+aux5) / 5;
+        printf("Average: %.2f\n", average);
+        students[studentIndex].average = average;
+        printf("Answer 1: %d\n", answer1);
+        students[studentIndex].answer1 = answer1;
+        printf("Answer 2: %d\n", answer2);
+        students[studentIndex].answer2 = answer2;
+        printf("Answer 3: %d\n", answer3);
+        students[studentIndex].answer3 = answer3;
+        studentIndex++;
+
     }
-    printf("\n");
+    printf("\n\n");
+
 }
-
-void storeData(FILE *database, struct StudentData *student, int MAX_RANDOM_LINES) {
-    char buffer[1024];
-    int count = 0;
-
-    fgets(buffer, sizeof(buffer), database); // Read and discard the header line
-
-    fgets(buffer, sizeof(buffer), database);
-    while (fgets(buffer, sizeof(buffer), database) && count < MAX_RANDOM_LINES) {
-        sscanf(buffer, "%d,%2s,%d,%c,%d,%f,%f,%f,%f,%f,%d,%f,%c",
-        &student[count]->NU_INSCRICAO, student[count]->SG_UF_RESIDENCIA,
-        &student[count]->NU_IDADE, &student[count]->TP_SEXO,
-        &student[count]->TP_COR_RACA, &student[count]->NU_NOTA_CN,
-        &student[count]->NU_NOTA_CH, &student[count]->NU_NOTA_LC,
-        &student[count]->NU_NOTA_MT, &student[count]->NU_NOTA_REDACAO,
-        &student[count]->Q005, &student[count]->Q006, &student[count]->Q025);
-        count++;
-    }
-}
-
-
-
-    }
